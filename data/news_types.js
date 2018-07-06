28613319549573aff2da228315866d70868576e2
Nikeweke <Driver3@meta.ua>
2018-07-06 16:20:23 +0300

    Create news_types.js

---
+++ data/news_types.js
@@ -0,0 +1,6 @@
+export default [
+    {clr: 'info',      icon: 'fas fa-cocktail',         name: 'День рождение'},
+    {clr: 'warning',   icon: 'fas fa-bullhorn',         name: 'Событие'},
+    {clr: 'success',   icon: 'fas fa-address-card',     name: 'Новый сотрудник'},
+    {clr: 'danger', icon: 'fas fa-users',            name: 'Собрание'},
+]
